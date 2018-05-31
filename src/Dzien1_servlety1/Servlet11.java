af1dc84c47436fe2aa404bb6c96d88359f09df57
Agata Stanislawska <agatasa96@gmail.com>
2018-05-31 12:06:11 +0200

    Wyswietlanie w przegladarce napisu

---
+++ src/Dzien1_servlety1/Servlet11.java
@@ -0,0 +1,18 @@
+package Dzien1_servlety1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlt11")
+public class Servlet11 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException{
+		
+		response.getWriter().append("<h1> Hello first servlet </h1>");
+	}
+}
