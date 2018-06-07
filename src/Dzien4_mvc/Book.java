ea2a32e824e90a02434d876fcbe7b7db1fe07bb5
Agata Stanislawska <agatasa96@gmail.com>
2018-06-07 21:49:15 +0200

    Klasa book zawierajaca parametry takie jak w formularzu mvc03_1.jsp oraz konstruktor i metode toString()

---
+++ src/Dzien4_mvc/Book.java
@@ -0,0 +1,48 @@
+package Dzien4_mvc;
+
+public class Book {
+	private String author;
+	private String title;
+	private String ISBN;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	@Override
+	public String toString() {
+		return "Book: author = " + author + ", title = " + title;
+	}
+
+	public Book(String author, String title, String iSBN) {
+		super();
+		this.author = author;
+		this.title = title;
+		ISBN = iSBN;
+	}
+
+	public Book() {
+
+	}
+
+}
